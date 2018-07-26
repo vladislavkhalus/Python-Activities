602d410e87d4a421824423d7735adaf72ed82598
vladislavkhalus <41482749+vladislavkhalus@users.noreply.github.com>
2018-07-26 10:42:29 -0400

    whitespaces

--- Activity1/Activity1.py
+++ Activity1/Activity1.py
@@ -1,13 +1,12 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 Spyder Editor
 
 This is a temporary script file.
 """
-
 array = []
 pelidrome_input = input("Input word to check for pelidrome ")
-divide2 = len(pelidrome_input)  
+divide2 = len(pelidrome_input)
 input_size = len(pelidrome_input)
 counter = 0
 for j in range(input_size):
@@ -17,6 +16,6 @@ for i in range(divide2):
     if array[i] != array[size_array-1-i]:
         counter = counter + 1
 if counter == 0:
-    print("It's a pelidrome") 
-else: 
-    print("It's not a pelidrome" )
+    print("It's a pelidrome\n")
+else:
+    print("It's not a pelidrome\n")
--- Activity2/Activity2.py
+++ Activity2/Activity2.py
@@ -3,39 +3,24 @@ import pandas as panda
 import matplotlib.pyplot as plt
 
 data_field1 = panda.read_csv('sensor.csv')
-
-
-# Importing the fields to arrays
-#signals = data_field1['strenght10']
-#time1 = data_field1['time10'] 
 x_value = data_field1['ORIENTATIONX']
-y_value = data_field1['ORIENTATIONY'] 
-a_value = data_field1['LIGHT'] 
-z_value = data_field1['ORIENTATIONZ'] 
+y_value = data_field1['ORIENTATIONY']
+a_value = data_field1['LIGHT']
+z_value = data_field1['ORIENTATIONZ']
 time = []
-
-#z_value = data_field1[ORIENTATIONZ]
-
-#multiplicationxy = (x_value**2+y_value**2)**(1.0/2)
-
 multiplicationxy = list()
- 
-for i in range(0,392,1):
-    multiplicationxy.append(math.sqrt(math.pow(x_value[i],2) + math.pow(y_value[i],2) )*10) #magnitude * 10
-
-for j in range(0,392,1):
+for i in range(0, 392, 1):
+    multiplicationxy.append(math.sqrt(math.pow(x_value[i], 2) +
+                            math.pow(y_value[i], 2))*10)
+for j in range(0, 392, 1):
     time.append(j)
-    
-# Visualizing the data
-#plot(X,Y,specifiers)
-#plt.plot(time2,speed,'b')
-plt.plot( time, multiplicationxy, 'r', label='XY Orientation')
-plt.plot( time, a_value, 'b', label='Light')
+plt.plot(time, multiplicationxy, 'r', label='XY Orientation')
+plt.plot(time, a_value, 'b', label='Light')
 plt.xlabel('Number of Samples')
-plt.ylabel('Light and Orientation') 
-# Insert title ('My Title - Speed vs Time') ?? 
-# Legend ??? 
+plt.ylabel('Light and Orientation')
 plt.title('Orientation of the Phone vs. Light')
 plt.legend(loc='upper left')
 plt.show()
+
+
 
